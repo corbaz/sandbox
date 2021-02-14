da46db8c3f7d3ee5fea0e11dfce51bfc9d18c73c
Julio Cesar Corbaz <julio.corbaz@gmail.com>
2021-02-14 08:20:57 -0300

    ForIn-ForOf JS Ejemplos For ...in For ...of

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+let arr = {
+  edad: 20,
+  ano: 1967
+}
+
+
+// elKey are the property keys
+for (let elKey in arr) {
+  console.log(elKey);
+}
+
+// elValue are the property values
+for (let elValue in arr) {
+  console.log(arr[elValue])
+
+}
+let ape = 'corbaz'
+let nom = 'julio'
+let obj = {
+  ape,
+  nom,
+  sexo: 'mas'
+}
+
+ape = 'guille'
+console.log(obj,ape)
+
+let numeros = [1, 3, 5, 7, 9];
+
+for (let i in numeros) {
+    console.log(i);     
+    // log -> 0, 1, 2, 3, 4 indices
+}
+let array = [33, 55, 77];
+array.test = "Beta";
+
+for (let i in array) {
+    console.log(i);     
+    // log -> "0", "1", "2", "test"
+}
+
+for (let i of array) {
+    console.log(i); 
+    // log -> "33", "55", "77"
+}
+
 const myObject = { name: 'kuworking', description: 'Aprende desarrollo web en kuworking.com' }
 
 for (const property in myObject) {
